52125876f83ec5b2cb9db5dbc0388c32465fe1b1
umasankar421 <buddi.umashankar@loginworks.com>
2016-12-15 19:24:19 +0530

    initial commit

---
+++ SearchFeatureDemo/SearchFeatureDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SearchFeatureDemo
+//
+//  Created by Vikas Mishra on 12/15/16.
+//  Copyright © 2016 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
