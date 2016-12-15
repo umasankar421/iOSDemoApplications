65e174c32aa7c25845c8e3bb8a8a1d4371224c14
umasankar421 <buddi.umashankar@loginworks.com>
2016-12-15 18:53:07 +0530

    Search Feature Demo
    Search Feature Demo Project is added.

---
+++ SearchFatureDemo/SearchFatureDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchFatureDemo
+//
+//  Created by Vikas Mishra on 12/15/16.
+//  Copyright © 2016 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SearchFatureDemo/SearchFatureDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SearchFatureDemo
+//
+//  Created by Vikas Mishra on 12/15/16.
+//  Copyright © 2016 dev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
