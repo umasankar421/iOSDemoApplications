a2934799cd49ffdf61c2d43ebae4d04664ac19f3
umasankar421 <buddi.umashankar@loginworks.com>
2016-12-16 13:57:21 +0530

    commit with simple implementation

--- ExpandableTableViewDemo/ExpandableTableViewDemo/ViewController.h
+++ ExpandableTableViewDemo/ExpandableTableViewDemo/ViewController.h
@@ -1,15 +1,19 @@
 //
 //  ViewController.h
-//  ExpandableTableViewDemo
+//  ExpandableTableView
 //
-//  Created by Vikas Mishra on 12/16/16.
+//  Created by Vikas Mishra on 10/14/16.
 //  Copyright © 2016 dev. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>{
+    NSMutableArray *visibleRows;
+    NSMutableArray *cellInfo;
+}
 
+@property (strong, nonatomic) IBOutlet UITableView *myTableView;
 
 @end
 
