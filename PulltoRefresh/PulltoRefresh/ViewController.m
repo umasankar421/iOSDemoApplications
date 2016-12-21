e19411b3c3b21dbd1d87eff9447e9d8efa6b538a
umasankar421 <buddi.umashankar@loginworks.com>
2016-12-21 14:40:45 +0530

    Code deletion

--- PulltoRefresh/PulltoRefresh/ViewController.m
+++ PulltoRefresh/PulltoRefresh/ViewController.m
@@ -22,13 +22,12 @@
     
     // Do any additional setup after loading the view, typically from a nib.
     
-    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc]initWithTitle:@"Click to Down" style:UIBarButtonItemStyleDone target:self action:@selector(scrolltoDown:)];
+    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc]initWithTitle:@"Scroll to Down" style:UIBarButtonItemStyleDone target:self action:@selector(scrolltoDown:)];
     [self.navigationItem setRightBarButtonItem:rightButton];
     btn_scrollUp.hidden = YES;
     
     tableData = [[NSArray alloc]init];
     tableData = @[@"Aakriti",@"Aamukta",@"Aanandita",@"Aarti",@"Abhaya",@"Bhaageerathi",@"Bhaargavi",@"Bhanu",@"Chandini",@"Chaaya",@"Damayanti",@"Deepika",@"Deepti",@"Divya",@"Ektaa",@"Esha",@"Fulki",@"Ganga",@"Gauri",@"Gautami",@"Geeta ",@"Hrishita",@"Indira",@"Indraja",@"Janaki",@"Jyotsana",@"Kalpana",@"Kamala",@"Kanchan Verma",@"Lakshmi",@"Lalitha",@"Madhavi",@"Madhu",@"Madhuri",@"Manju",@"Nandini",@"Nilima",@"Nirmala",@"Oormila",@"Padma",@"Parvati",@"Pooja",@"Priya",@"Radha",@"Raksha",@"Rishita",@"Sandhya",@"Saraswati",@"Savitri",@"Sharmila",@"Shobhana",@"Uma Shankar",@"Upasana",@"Vaishali",@"Yamuna",@"Women",@"Xanshai",@"Zampa",@"Zameera",@"Zain",@"Trisha",@"Queen",@"Vani",@"Vanaja"];
-    
 }
 
 
@@ -38,19 +37,11 @@
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self scrollToTheBottom:YES];
             [self.navigationItem.rightBarButtonItem setTitle:@"Scroll to Up"];
-            
-            //btn_scrolltoDown.hidden = YES;
-            //btn_scrolltoUp.hidden = NO;
-            
         });
     }else{
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self scrollToTheTop:YES];
             [self.navigationItem.rightBarButtonItem setTitle:@"Scroll to Down"];
-            
-            //btn_scrolltoDown.hidden = YES;
-            //btn_scrolltoUp.hidden = NO;
-            
         });
     }
 }
@@ -65,7 +56,6 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self scrollToTheTop:YES];
         [self.navigationItem.rightBarButtonItem setTitle:@"Scroll to Down"];
-        //btn_scrolltoUp.hidden = NO;
     });
 }
 - (void)scrollToTheTop:(BOOL)animated{
@@ -75,15 +65,6 @@
 }
 
 -(void)scrollViewDidScroll:(UIScrollView *)scrollView{
-//    NSArray *visibleRows = [self.refreshTableView visibleCells];
-//    UITableViewCell *lastVisibleCell = [visibleRows lastObject];
-//    NSIndexPath *path = [self.refreshTableView indexPathForCell:lastVisibleCell];
-//    
-//    if(path.row == [tableData count]-1)
-//    {
-//        btn_scrollUp.hidden = NO;
-//    }
-    
     if (scrollView.contentOffset.y == 0) {
         btn_scrollUp.hidden = YES;
         [self.navigationItem.rightBarButtonItem setTitle:@"Scroll to Down"];
